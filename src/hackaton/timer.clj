36163e798327dc959ff9b445ff3b77652e1dc11c
Magnus Dreyer <magnus.dreyer@ufst.dk>
2021-07-16 09:24:42 +0200

    Skrevet lide pseudokode til timere

--- src/hackaton/timer.clj
+++ src/hackaton/timer.clj
@@ -3,3 +3,19 @@
 
 "Ideen med timeren er, at den hvert sekund, eller måske bare hvert femte,  sender et 'tick' ud til systemet, som
 andre tråde så abonnerer på, og når der er kommet et 'tick', vil de så udføre deres opgave og så sove indtil næste 'tick' "
+
+
+(def tick (atom 0))
+
+
+(def ventetid (atom 1000)) ;; Antal millisekunder som timeren skal sove, inden den sendet et nyt tick ud
+
+(defn start-timer
+  [x]
+  (while (< @tick 10000)
+    (do
+      (sleep @ventetid)
+      (swap! tick inc)
+      )
+    )
+  )
