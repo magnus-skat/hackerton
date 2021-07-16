7453bbd2422faa835d42c2fffa2e265b07b339f8
Magnus Dreyer <magnus.dreyer@ufst.dk>
2021-07-16 11:06:29 +0200

    Opdateret kommentarer

--- src/hackaton/core.clj
+++ src/hackaton/core.clj
@@ -5,12 +5,21 @@
             [hackaton.timer :as timer]
             ))
 
+"For at kører dette på dit eget system, så skal du starte en REPL og så kører
+start-system. Der bliver så oprettet en dæmning, som hvert sekund vil skrive noget ud
+på skærmen
+Det er muligt at ændre tiden imellem ticks i realtime ved at køre
+(reset! hackaton.timer/ventetid 2000)
+i din REPL
+
+"
+
 (def system (atom {}))
 
 (defn init-system
   [_]
   {
-   :ventetid-mellem-ticks-i-millisekunder 1000
+   :ventetid-mellem-ticks-i-millisekunder 1000 ;; Bliver ikke brugt lige nu
    :dæmninger                             []
    :andre-ting                            :som-jeg-har-glemt
    })
