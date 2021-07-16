1c2ea394da9c918489794948689e4d4461314597
Magnus Dreyer <magnus.dreyer@ufst.dk>
2021-07-16 11:47:16 +0200

    dæmning ændringer

--- src/hackaton/daemning.clj
+++ src/hackaton/daemning.clj
@@ -3,16 +3,15 @@
             [hackaton.skov :as skov]
             [hackaton.timer :as timer]
             ))
-"https://www.tutorialspoint.com/clojure/clojure_watchers.htm "
+"https://www.tutorialspoint.com/clojure/clojure_watchers.htm"
 
 (defn a-queue
   ([] ((pq/priority-queue #(:error %))))
   ([coll]
-   (let [kø (pq/priority-queue #(:error %)) ]
+   (let [kø (pq/priority-queue #(:error %))]
      reduce conj kø coll)))
 
-(def min-kø (a-queue [(skov/fæld-træ 1) (skov/fæld-træ 1) (skov/fæld-træ 1) (skov/fæld-træ 1) (skov/fæld-træ 1) ]))
-
+(def min-kø (a-queue []))
 
 (defn ticker-har-ticket
   [key atom old-state new-state]
@@ -21,6 +20,10 @@
   (println "Ny værdi" new-state)
   )
 
+
 (defn example []
-  (add-watch timer/tick :watcher
-             ticker-har-ticket))
+  (let [
+        min-kø (a-queue)
+        ]
+    (add-watch timer/tick :watcher
+               ticker-har-ticket)))
